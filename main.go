a860017f65feabd4b78f1db2120df3f1fcefbfbb
anthdm <cryptoanthdm@gmail.com>
2024-04-07 09:40:15 +0200

    initial

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log/slog"
+	"net/http"
+)
+
+func main() {
+	router := http.NewServeMux()
+	port := ":3000"
+	router.HandleFunc("/up", handleGetUp)
+
+	slog.Info("running application", "port", port)
+
+	http.ListenAndServe(port, router)
+}
+
+func handleGetUp(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("it's up!"))
+}
